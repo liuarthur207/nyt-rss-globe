dbe408e9eed61956af6892f10e940d9497f674f1
arthurl5@illinois.edu <liuarthur11@gmail.com>
2025-03-07 01:49:51 -0600

    switching to just meshes bc instanced mesh was too hard for me :(

--- classes/NewsMarker.js
+++ classes/NewsMarker.js
@@ -3,24 +3,24 @@ import * as THREE from 'three'
 export default class NewsMarker extends THREE.Mesh {
     constructor(country) {
       super()
-      this.geometry = new THREE.PlaneGeometry(0.1, 0.1)
-      this.material = new THREE.MeshBasicMaterial({ color: 'orange'})
+      this.geometry = new THREE.CircleGeometry(0.015)
+      this.material = new THREE.MeshBasicMaterial({ color: 'red'})
       this.size = 0
       this.active = false
       this.country = country
     }
   
     onResize(width, height, aspect) {
-      this.cubeSize = width / 5 // 1/5 of the full width
+      this.size = width / 5 // 1/5 of the full width
       this.scale.setScalar(this.size * (this.active ? 1.5 : 1))
     }
   
     onPointerOver(e) {
-      this.material.color.set('hotpink')
+      this.material.color.set('orange')
     }
   
     onPointerOut(e) {
-      this.material.color.set('orange')
+      this.material.color.set('red')
     }
   
     onClick(e) {
